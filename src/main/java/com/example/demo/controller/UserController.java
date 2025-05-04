5256b5c28e591721efa86d8f4ee38e238d86060c
fhpthh <huehongphan0807@gmail.com>
2025-05-04 21:10:09 +0700

    user

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -5,42 +5,42 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.domain.User;
 import com.example.demo.service.UserService;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class UserController {
 
     private UserService userService;
-    
-     public UserController(UserService userService) {
-         this.userService = userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
+
     @RequestMapping("/")
     public String getHomePage(Model model) {
         String test = this.userService.handleHello();
         model.addAttribute("eric", test);
         model.addAttribute("pthh", "PhanThiHongHue");
         return "hello";
-
     }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        String test = this.userService.handleHello();
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User pthh) {
+        System.out.println(pthh);
+        return "hello";
+    }
+
 }
-
-// @RestController
-// public class UserController {
-
-//     private UserService userService;
-    
-//     public UserController(UserService userService) {
-//         this.userService = userService;
-//     }
-
-//     @GetMapping("")
-//     public String getHomePage() {
-//         return this.userService.handleHello();
-//     }
-    
-// }
