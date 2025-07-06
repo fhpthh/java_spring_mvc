88702210eada11c5628c14b14912e843886ad135
fhpthh <huehongphan0807@gmail.com>
2025-07-06 15:23:39 +0700

    done chap10

--- src/main/java/com/example/demo/domain/Product.java
+++ src/main/java/com/example/demo/domain/Product.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +22,8 @@ public class Product {
     private long sold;
     private String factory;
     private String target;
+
+
     public long getId() {
         return id;
     }
